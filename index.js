e823168564df9895c0ea6c0fa562492c5b25a801
Izaack Kuria <kuriaisac@gmail.com>
2024-06-27 21:08:56 +0300

    Adds scopes

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = "bob";
+// function that access a global variable and uppercases it
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+// function to declare a variable in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+setBestCustomer();
+// function that changes a global scope variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+overwriteBestCustomer();
+// constant in the global scope
+const leastFavoriteCustomer = "value";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "intial";
+}
+changeLeastFavoriteCustomer();
